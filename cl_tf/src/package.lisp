286a038c5141e238d246273fa349dc6013febe8f
lorenz <lorenz@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2010-08-18 10:09:19 +0000

    Package exports
    git-svn-id: https://code.ros.org/svn/ros-pkg/branches/trunk_cturtle/stacks/roslisp_common@31807 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

--- cl_tf/src/package.lisp
+++ cl_tf/src/package.lisp
@@ -2,4 +2,7 @@
   (:use :cl :roslisp :tf-msg :cl-transforms)
   (:shadow :transform-pose :transform-point)
   (:export :transformer :make-transformer
-           :transform-listener))
+           :transform-listener
+           :can-transform :lookup-transform
+           :set-transform :transform-pose
+           :transform-point))
