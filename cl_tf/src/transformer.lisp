938144c6d08d48c92257cb35515be350aec19417
lorenz <lorenz@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2010-09-11 11:32:43 +0000

    Fully integrated transform-cache into transformer
    git-svn-id: https://code.ros.org/svn/ros-pkg/branches/trunk_cturtle/stacks/roslisp_common@32588 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

--- cl_tf/src/transformer.lisp
+++ cl_tf/src/transformer.lisp
@@ -20,12 +20,11 @@
 (defgeneric transform-point (tf &key target-frame point))
 
 (defmethod can-transform ((tf transformer) &key target-frame source-frame time)
-  (declare (ignore time))
   (check-type target-frame string)
   (check-type source-frame string)
   (or (equal target-frame source-frame)
-      (let ((target-root (get-transforms-along-path (transforms tf) target-frame))
-            (source-root (get-transforms-along-path (transforms tf) source-frame)))
+      (let ((target-root (get-transforms-to-root (transforms tf) target-frame time))
+            (source-root (get-transforms-to-root (transforms tf) source-frame time)))
         (cond ((and target-root source-root)
                (equal (frame-id (car target-root))
                       (frame-id (car source-root))))
@@ -44,8 +43,8 @@
       (make-stamped-transform target-frame source-frame (ros-time)
                               (make-3d-vector 0 0 0)
                               (make-quaternion 0 0 0 1))))
-  (let* ((down-transforms (get-transforms-along-path (transforms tf) target-frame))
-         (up-transforms (get-transforms-along-path (transforms tf) source-frame)))
+  (let* ((down-transforms (get-transforms-to-root (transforms tf) target-frame time))
+         (up-transforms (get-transforms-to-root (transforms tf) source-frame time)))
     (let ((result-tf (cond ((and down-transforms up-transforms)
                             (apply #'transform* (transform-inv (apply #'transform* down-transforms))
                                    up-transforms))
@@ -56,43 +55,53 @@
       (unless result-tf
         (error 'tf-connectivity-error :source-frame source-frame :target-frame target-frame))
       (make-stamped-transform target-frame source-frame
-                              (or time (gethash source-frame (slot-value tf 'transforms)))
+                              (or time (stamp (car (last up-transforms))))
                               (translation result-tf)
                               (rotation result-tf)))))
 
 (defmethod set-transform ((tf transformer) transform)
   (check-type transform stamped-transform)
   (with-slots (transforms) tf
-    (setf (gethash (child-frame-id transform) transforms)
-          transform)))
+    (let ((cache (gethash (child-frame-id transform) transforms)))
+      (unless cache
+        (setf cache (make-instance 'transform-cache))
+        (setf (gethash (child-frame-id transform) transforms) cache))
+      (cache-transform cache transform))))
 
-(defmethod transform-pose ((tf transformer) &key target-frame pose)
+(defmethod transform-pose ((tf transformer) &key target-frame pose time)
   (check-type target-frame string)
   (check-type pose pose-stamped)
   (let ((transform (lookup-transform tf
                                      :target-frame target-frame
-                                     :source-frame (frame-id pose))))
+                                     :source-frame (frame-id pose)
+                                     :time time)))
     (assert transform () "Transform from `~a' to `~a' not found."
             (frame-id pose) target-frame)
     (change-class (cl-transforms:transform-pose transform pose)
-                  'pose-stamped :frame-id target-frame)))
+                  'pose-stamped
+                  :frame-id target-frame
+                  :stamp (stamp transform))))
 
-(defmethod transform-point ((tf transformer) &key target-frame point)
+(defmethod transform-point ((tf transformer) &key target-frame point time)
   (check-type target-frame string)
   (check-type point point-stamped)
   (let ((transform (lookup-transform tf
                                      :target-frame target-frame
-                                     :source-frame (frame-id point))))
+                                     :source-frame (frame-id point)
+                                     :time time)))
     (assert transform () "Transform from `~a' to `~a' not found."
             (frame-id point) target-frame)
     (change-class (cl-transforms:transform-point transform point)
-                  'point-stamped :frame-id target-frame)))
+                  'point-stamped
+                  :frame-id target-frame
+                  :stamp (stamp transform))))
 
-(defun get-transforms-along-path (transforms frame-id &optional result)
+(defun get-transforms-to-root (transforms frame-id time &optional result)
   "Returns the list of transforms from `frame-id' up to the root of
   the tree."
-  (let ((current-tf (gethash frame-id transforms)))
-    (if current-tf
-        (get-transforms-along-path transforms (frame-id current-tf)
-                                   (cons current-tf result))
+  (let ((current-cache (gethash frame-id transforms)))
+    (if current-cache
+        (let ((current-tf (get-cached-transform (gethash frame-id transforms) time)))
+          (get-transforms-to-root transforms (frame-id current-tf)
+                                  time (cons current-tf result)))
         result)))
