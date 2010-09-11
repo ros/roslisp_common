3f8b5474839162e5ede2366414e6ff94f2df128b
lorenz <lorenz@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2010-09-11 11:32:30 +0000

    Added destroy method to transform-listener
    Currently it is disabled, but after the next ros release we can enable it since
    it requires unsubscribe.
    
    git-svn-id: https://code.ros.org/svn/ros-pkg/branches/trunk_cturtle/stacks/roslisp_common@32586 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

--- cl_tf/src/transform-listener.lisp
+++ cl_tf/src/transform-listener.lisp
@@ -4,6 +4,8 @@
 (defclass transform-listener (transformer)
   ((subscriber)))
 
+(defgeneric destroy (obj))
+
 (defmethod initialize-instance :after ((tf transform-listener) &key)
   (setf (slot-value tf 'subscriber)
         (subscribe "/tf" "tf/tfMessage" (lambda (msg) (tf-listener-callback tf msg)))))
@@ -12,3 +14,7 @@
   (mapc (lambda (transform)
           (set-transform tf transform))
         (tf-message->transforms msg)))
+
+(defmethod destroy ((tf transform-listener))
+  ;; (roslisp:unsubscribe (slot-value tf 'subscriber))
+  nil)
