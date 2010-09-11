7511cbc40746c41ba757f15b9955d89e67d518df
lorenz <lorenz@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2010-09-11 16:33:01 +0000

    Re-export symbols of cl-transforms in cl-tf
    git-svn-id: https://code.ros.org/svn/ros-pkg/branches/trunk_cturtle/stacks/roslisp_common@32597 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

--- cl_tf/src/package.lisp
+++ cl_tf/src/package.lisp
@@ -1,18 +1,21 @@
 
 (in-package :cl-user)
 
-(defpackage :cl-tf
-  (:use :cl :roslisp :tf-msg :cl-transforms)
-  (:nicknames :tf)
-  (:shadow :transform-pose :transform-point)
-  (:export :transformer :make-transformer
-           :transform-listener
-           :can-transform :lookup-transform
-           :set-transform :transform-pose
-           :transform-point
-           :stamped-transform :pose-stamped :point-stamped
-           :make-pose-stamped :make-point-stamped
-           :make-stamped-transform :transform->stamped-transform
-           :tf-transform->transform :tf-message->transforms
-           :stamped :stamped-transform :pose-stamped :point-stamped
-           :frame-id :stamp :child-frame-id))
+#.`(defpackage :cl-tf
+       (:use :cl :roslisp :tf-msg :cl-transforms)
+     (:nicknames :tf)
+     (:shadow :transform-pose :transform-point)
+     (:export :transformer :make-transformer
+              :transform-listener
+              :can-transform :lookup-transform
+              :set-transform :transform-pose
+              :transform-point
+              :stamped-transform :pose-stamped :point-stamped
+              :make-pose-stamped :make-point-stamped
+              :make-stamped-transform :transform->stamped-transform
+              :tf-transform->transform :tf-message->transforms
+              :stamped :stamped-transform :pose-stamped :point-stamped
+              :frame-id :stamp :child-frame-id
+              ,@(let ((r nil))
+                  (do-external-symbols (s :cl-transforms r) (push s r)))))
+
