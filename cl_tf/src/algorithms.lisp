faa7a71ed51e963434b686ae09e1e03cbc7f7b4f
lorenz <lorenz@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2010-09-11 11:32:51 +0000

    Fixed bugs in BINARY-SEARCH
    * The wrong variable was used. Do not use the array entry but use its index.
    * Handle corner cases where the requested value is outside the range between
      the first and the last array element.
    
    git-svn-id: https://code.ros.org/svn/ros-pkg/branches/trunk_cturtle/stacks/roslisp_common@32589 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

--- cl_tf/src/algorithms.lisp
+++ cl_tf/src/algorithms.lisp
@@ -2,7 +2,8 @@
 (in-package :cl-tf)
 
 (defun binary-search (value array &key
-                      (array-size (array-dimension array 0))
+                      (start 0)
+                      (end (1- (array-dimension array 0)))
                       (lt #'<) (key #'identity))
   "Performs a binary search for `value' in `array'. This function
 assumes that `array' is ordered with respect predicate `lt'.
@@ -12,22 +13,24 @@ value if the second parameter is greater than the first parameter.
 
 Returns two values, the last value that is `lt' than `value and the
 first `value' that is not `lt' than `value'."
-  (check-type array (array * (*)))
+  (declare (type (simple-array * 1) array))
   (check-type lt function)
-  (let ((i 0))
-    (labels ((perform-search (lower upper)
-               (incf i)
-               (if (<= (- upper lower) 1)
-                   (values (aref array lower)
-                           (aref array upper))
-                   (let* ((pivot-index (+ lower (truncate (/ (- upper lower) 2))))
-                          (pivot (aref array pivot-index)))
-                     (if (funcall lt value (funcall key pivot))
-                         (perform-search lower pivot)
-                         (perform-search pivot upper))))))
-      (multiple-value-call #'values
-        (perform-search 0 (1- array-size))
-        i))))
+  (labels ((perform-search (lower upper)
+             (if (<= (- upper lower) 1)
+                 (values (aref array lower)
+                         (aref array upper))
+                 (let* ((pivot-index (+ lower (truncate (/ (- upper lower) 2))))
+                        (pivot (aref array pivot-index)))
+                   (if (funcall lt value (funcall key pivot))
+                       (perform-search lower pivot-index)
+                       (perform-search pivot-index upper))))))
+    (cond ((funcall lt value (funcall key (aref array start)))
+           nil)
+          ((funcall lt (funcall key (aref array end)) value)
+           nil)
+          (t
+           (multiple-value-call #'values
+             (perform-search start end))))))
 
 (defun lower-bound (value array &key (lt #'<) (key #'identity))
   (multiple-value-bind (lower upper)
