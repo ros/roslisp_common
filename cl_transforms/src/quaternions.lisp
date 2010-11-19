ca35c5f9abb3cba728e61940d6d6149aaf4fa30a
lorenz <lorenz@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2010-11-19 09:32:17 +0000

    Removed some method specializations for type VECTOR
    The methods X, Y and Z are already defined for the type VECTOR in
    3d-vector.lisp. Re-defining them with exactly the same code in quaternion.lisp
    just causes another compiler warning. I removed the methods from
    quaternion.lisp since they get defined anyway.
    
    
    git-svn-id: https://code.ros.org/svn/ros-pkg/stacks/roslisp_common/trunk@33986 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

--- cl_transforms/src/quaternions.lisp
+++ cl_transforms/src/quaternions.lisp
@@ -12,15 +12,6 @@
    (w :initarg :w :reader w :type quaternion-coefficient))
   (:documentation "Represents a quaternion w, (x, y, z).  Must be treated as immutable.  The quaternion objects can take in real numbers as well, which are implicitly converted to a quaternion."))
 
-(defmethod x ((q vector))
-  (aref q 0))
-
-(defmethod y ((q vector))
-  (aref q 1))
-
-(defmethod z ((q vector))
-  (aref q 2))
-
 (defmethod w ((q vector))
   (aref q 3))
 
