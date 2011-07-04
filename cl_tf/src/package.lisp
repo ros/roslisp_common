5ad521bd92312798be81d7b94da771cfe5656001
lorenz <lorenz@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2011-07-04 12:08:32 +0000

    updated package exports
    git-svn-id: https://code.ros.org/svn/ros-pkg/stacks/roslisp_common/trunk@37404 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

--- cl_tf/src/package.lisp
+++ cl_tf/src/package.lisp
@@ -4,23 +4,24 @@
 #.`(defpackage :cl-tf
        (:use :cl :roslisp :tf-msg :cl-transforms)
      (:nicknames :tf)
-     (:shadow :transform-pose :transform-point)
-     (:export :transformer :make-transformer
-              :transform-listener
-              :can-transform :lookup-transform
-              :set-transform :transform-pose
-              :transform-point
-              :stamped-transform :pose-stamped :point-stamped
-              :make-pose-stamped :make-point-stamped
-              :make-stamped-transform :transform->stamped-transform
-              :tf-transform->transform :tf-message->transforms
-              :copy-pose-stamped
-              :msg->pose :msg->pose-stamped :pose-stamped->msg :pose->msg
-              :stamped :stamped-transform :pose-stamped :point-stamped
-              :wait-for-transform :tf-cache-error :tf-connectivity-error
-              :tf-lookup-error
-              :frame-id :stamp :child-frame-id :source-frame :target-frame
-              :frame
+     (:shadow transform-pose transform-point)
+     (:export transformer make-transformer
+              transform-listener
+              can-transform lookup-transform
+              set-transform transform-pose
+              transform-point
+              stamped-transform pose-stamped point-stamped
+              make-pose-stamped make-point-stamped
+              make-stamped-transform transform->stamped-transform
+              tf-transform->transform tf-message->transforms
+              copy-pose-stamped msg->pose msg->pose-stamped
+              pose-stamped->msg pose->msg stamped stamped-transform
+              pose-stamped point-stamped wait-for-transform
+              tf-cache-error tf-connectivity-error tf-lookup-error
+              frame-id stamp child-frame-id source-frame target-frame
+              frame
+              make-transform-broadcaster send-transform
+              send-static-transform-blocking send-static-transform
               ,@(let ((r nil))
                   (do-external-symbols (s :cl-transforms r) (push s r)))))
 
