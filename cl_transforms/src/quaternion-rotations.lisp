0a44060a93869e0f978e052ace52aae6f1f2ed21
lorenz <lorenz@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2011-08-08 15:42:35 +0000

    Added function NORMALIZE-ANGLE
    git-svn-id: https://code.ros.org/svn/ros-pkg/stacks/roslisp_common/trunk@37722 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

--- cl_transforms/src/quaternion-rotations.lisp
+++ cl_transforms/src/quaternion-rotations.lisp
@@ -108,3 +108,16 @@ This guarantees that Q represents a rotation."
   (multiple-value-bind (axis angle)
       (quaternion->axis-angle (q* (q-inv q1) q2))
     (values angle axis)))
+
+(defun normalize-angle (angle)
+  "Ensures that `angle' is within the interval [0;PI)."
+  (let ((2pi (* pi 2)))
+    (cond ((< angle 0)
+           (normalize-angle
+            (- angle  (* (1- (truncate (/ angle 2pi)))
+                         2pi))))
+          ((>= angle 2pi)
+           (normalize-angle
+            (- angle (* (truncate (/ angle 2pi))
+                        2pi))))
+          (t angle))))
