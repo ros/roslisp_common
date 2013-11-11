e89527c24513b927bce92105685f4b9504093945
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2013-11-11 20:00:57 +0100

    Refactored datatypes in cl_tf2: No more inheritance.

--- cl_tf2/src/datatypes.lisp
+++ cl_tf2/src/datatypes.lisp
@@ -1,16 +1,18 @@
 (in-package :cl-tf2)
 
-(defclass stamped ()
+(defclass header ()
   ((frame-id :initarg :frame-id :reader frame-id :type string)
    (stamp :initarg :stamp :reader stamp :type float)))
 
-(defclass stamped-transform (stamped)
-  ((transform :initarg :transform :reader transform)
+(defclass stamped-transform ()
+  ((header :initarg :header :reader header)
+   (transform :initarg :transform :reader transform)
    (child-frame-id :initarg :child-frame-id :reader child-frame-id :type string)))
 
 (defun make-stamped-transform (frame-id child-frame-id stamp transform)
   (make-instance 'stamped-transform
-                 :frame-id frame-id
+                 :header (make-instance 'header
+                                        :frame-id frame-id
+                                        :stamp stamp)
                  :child-frame-id child-frame-id
-                 :stamp stamp
                  :transform transform))
