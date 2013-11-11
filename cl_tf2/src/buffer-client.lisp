33869791341e134a919c71ecd650a1c8728dd1aa
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2013-11-11 10:29:42 +0100

    Another try at a generic interface for tf2.

--- cl_tf2/src/buffer-client.lisp
+++ cl_tf2/src/buffer-client.lisp
@@ -8,3 +8,17 @@
            :reader client)
    (frequency :initarg :frequency :initform 10 :reader frequency)
    (timeout-padding :initarg :timeout-padding :initform 2 :reader timeout-padding)))
+
+(defmethod can-transform ((tf buffer-client) target-frame source-frame 
+                          &optional (source-time 0.0) (timeout 0.0)
+                            (target-time source-time target-time-supplied-p)
+                            (fixed-frame source-frame fixed-frame-supplied-p))
+  (declare (ignore tf target-frame source-frame source-time timeout
+                   target-time target-time-supplied-p fixed-frame fixed-frame-supplied-p)))
+
+(defmethod lookup-transform ((tf buffer-client) target-frame source-frame 
+                             &optional (source-time 0.0) (timeout 0.0) 
+                               (target-time source-time target-time-supplied-p)
+                               (fixed-frame source-frame fixed-frame-supplied-p))
+  (declare (ignore tf target-frame source-frame source-time timeout target-time
+                   target-time-supplied-p fixed-frame fixed-frame-supplied-p)))
--- cl_tf2/src/buffer-interface.lisp
+++ cl_tf2/src/buffer-interface.lisp
@@ -1,7 +1,13 @@
 (in-package :cl-tf2)
 
-(defgeneric can-transform (tf target-frame source-frame source-time timeout
-                                &optional target-time fixed-frame))
+(defgeneric can-transform (tf target-frame source-frame &optional source-time timeout
+                                                          target-time fixed-frame)
+  (:documentation "Queries whether 'tf' is aware of a transform from 'source-frame' to 'target-frame'.
 
-(defgeneric lookup-transform (tf target-frame source-frame source-time timeout
-                                   &optional target-time fixed-frame))
+Optionally, one can specify a 'timeout' for the query. 'source-time' and 'target-time' specify at which time 'target-frame' and 'source-frame' should be evaluated. 'fixed-frame' denotes the frame in which the transform is assumed to be constant over time. Note: If 'target-time' is specified once also needs to specify 'fixed-frame' to avoid a run-time error."))
+
+(defgeneric lookup-transform (tf target-frame source-frame &optional source-time timeout
+                                                             target-time fixed-frame)
+  (:documentation "Queries 'tf' for the transform from 'source-frame' to 'target-frame'.
+
+Optionally, one can specify a 'timeout' for the query. 'source-time' and 'target-time' specify at which time 'target-frame' and 'source-frame' should be evaluated. 'fixed-frame' denotes the frame in which the transform is assumed to be constant over time. Note: If 'target-time' is specified once also needs to specify 'fixed-frame' to avoid a run-time error."))
