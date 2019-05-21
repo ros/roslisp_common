c617f1209f3ffb60aceacde8a8f7954c3b459260
Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
2019-05-21 21:10:03 +0200

    [cl-urdf] also exported MULTIPLIER and OFFSET symbols

--- cl_urdf/src/package.lisp
+++ cl_urdf/src/package.lisp
@@ -34,7 +34,7 @@
   (:use #:common-lisp)
   (:export
    upper lower effort velocity joint-type
-   joint type name axis origin parent child limits mimics
+   joint type name axis origin parent child limits mimics multiplier offset
    inertial mass inertia
    geometry box size cylinder radius length cylinder-length sphere
    mesh filename scale
